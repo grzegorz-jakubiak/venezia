0fe1b3d384c30e50ce22855105518475574234e1
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2021-10-19 21:36:48 +0200

    Refactor parser tests

--- test/legaseriea/parser_test.exs
+++ test/legaseriea/parser_test.exs
@@ -2,38 +2,34 @@ defmodule LegaSerieA.ParserTest do
   use ExUnit.Case
   import Tesla.Mock
 
-  setup_all do
-    {:ok, html} = Path.join([__DIR__, "../parser_fixture.html"]) |> Path.absname() |> File.read()
+  setup %{url: url, filename: filename} do
+    {:ok, html} = Path.join([__DIR__, "../fixtures", filename]) |> Path.absname() |> File.read()
     {:ok, parsed_html} = Floki.parse_document(html)
-    [html: html, parsed_html: parsed_html]
-  end
-
-  setup context do
-    %{url: url, html: html} = context
 
     mock(fn
       %{method: :get, url: ^url} ->
         %Tesla.Env{status: 200, body: html}
     end)
 
-    :ok
+    {:ok, parsed_html: parsed_html}
   end
 
   describe "find_match_box_by_team" do
     @describetag url: "https://example.com"
+    @describetag filename: "matches_fixture.html"
 
     test "finds the box", %{url: url, parsed_html: parsed_html} do
-      [_ | t] = parsed_html
+      [_ | t] = parsed_html |> Floki.find(".box-partita")
       assert t == LegaSerieA.Parser.find_match_box_by_team(url, "venezia")
     end
 
     test "is case insensitive", %{url: url, parsed_html: parsed_html} do
-      [_ | t] = parsed_html
+      [_ | t] = parsed_html |> Floki.find(".box-partita")
       assert t == LegaSerieA.Parser.find_match_box_by_team(url, "VENEZIA")
     end
 
     test "returns empty list when it's not there", %{url: url, parsed_html: parsed_html} do
-      [_ | t] = parsed_html
+      [_ | t] = parsed_html |> Floki.find(".box-partita")
       refute t == LegaSerieA.Parser.find_match_box_by_team(url, "test")
       assert [] == LegaSerieA.Parser.find_match_box_by_team(url, "test")
     end
